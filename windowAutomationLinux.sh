7fedc34d69529a04ee818ea585ff0a0a948ad3dc
Piyush Choudhary <piyushchoudhary.choudhary1@gmail.com>
2017-05-23 02:53:23 +0530

    Create windowAutomationLinux.sh
    Windows in linux work on window-ids. These functions are for getting those ids and then commands for maximizing and minimizing those windows.

---
+++ windowAutomationLinux.sh
@@ -0,0 +1,4 @@
+xwininfo -display :0  {get window id by clicking on window}
+xwininfo -name <window-name>  {get window id by window name} 
+xdotool windowminimize <window-id>  {minimize window}
+wmctrl -ia <window-id>  {maximize window}
